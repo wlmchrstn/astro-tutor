8d50b89c4fadccab88e34e96ab73e310241c0e10
wlmchrstn <wlmchrstn@gmail.com>
2026-04-29 00:44:18 +0700

    add preact library and astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://wlmchrstn-astro-tutor.netlify.app"
+  site: "https://wlmchrstn-astro-tutor.netlify.app",
+  integrations: [preact()]
 });
